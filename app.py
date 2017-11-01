e018af0a568afa73af86cbaa49989a4d8e8b0d6e
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2017-11-01 17:32:46 +0300

    using template from https://github.com/plotly/dash-heroku-template/blob/master/app.py

--- app.py
+++ app.py
@@ -25,7 +25,12 @@ import requests
 import urllib.parse
 
 
-app = dash.Dash()
+# Setup the app
+# Make sure not to change this file name or the variable names below,
+# the template is configured to execute 'server' on 'app.py'
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash(__name__, server=server)
 
 
 # NOT TODO: may be a class Data with 
@@ -172,6 +177,9 @@ def update_graph_parameters(freq, name1, name2):
 #
 
 
+#if __name__ == '__main__':
+#    app.run_server(debug=True)           
+# Run the Dash app
 if __name__ == '__main__':
-    app.run_server(debug=True)           
+    app.server.run(debug=True, threaded=True)
     
