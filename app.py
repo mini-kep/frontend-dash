cbb3830ac4fecba213846f7f9583b75bdb48c533
=epogrebnyak <e.pogrebnyak@gmail.com>
2017-11-05 15:38:38 +0300

    update plot view and controls

--- app.py
+++ app.py
@@ -14,9 +14,32 @@ Scenario:
 4. select indicator 2 by name in drop-down menu 
   -> choosing name affects plot and download footer 
 
+Additional functionality:
+    
+5. Slider controls date source range and axis range. When changing 
+   source the Y axis changes as well.    
+
 """
-import os
+
+"""
+NOT IMPLMENTED:
+    
+1. Group selector added before each variable name selector.
+
+2. One link for csv with two variables (requires API change or
+   merging dataframes, a bit harder for daily data). 
+
+3. Annotation for the last observation value.
+
+4. Names description of the variables - must have some text field.
+
+5. Footer formatting.
+
+"""
+
+from datetime import datetime  
 from random import randint
+import os
 
 import flask
 import dash
@@ -28,18 +51,72 @@ import requests
 
 
 # Setup from <https://github.com/plotly/dash-heroku-template>
-# ...the template is configured to execute 'server' on 'app.py'
+# > the template is configured to execute 'server' on 'app.py'
 server = flask.Flask(__name__)
 server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
 app = dash.Dash(__name__, server=server)
+
+# app properties
 app.css.append_css({"external_url": "https://codepen.io/anon/pen/LONErz.css"})
 app.title = 'mini-kep browser'
 
 
-# NOT TODO: may be a class Source with 
-# - Source.names() 
-# - Source.time_series(freq, name) 
-# - Source.frequencies()
+BASE_URL = 'http://minikep-db.herokuapp.com/api'
+
+class VariableNames:
+    def __init__(self, freq):
+        self.url = f'{BASE_URL}/names/{freq}'
+        
+    def get_from_api(self):
+        return requests.get(self.url).json()
+    
+    def as_menu_items(self):
+        names = self.get_from_api()
+        return [{'label': name, 'value': name} for name in names]   
+
+class DataSeries:
+    def __init__(self, freq, name):
+        self.url = self.make_url(freq, name, 'json')
+        self.freq = freq
+        self.name = name
+        
+    @staticmethod
+    def make_url(freq, name, format):
+        return (f'{BASE_URL}/datapoints'
+                f'?freq={freq}&name={name}&format={format}')    
+    
+    def get_from_api(self):
+        data = requests.get(self.url).json()
+        if not isinstance(data, list):
+             # if parameters are invalid, response is not a jsoned list
+             return []
+        return data
+
+    @staticmethod
+    def get_year(datapoint):
+        return datetime.strptime(datapoint['date'], "%Y-%m-%d").year
+
+    def get_dict(self, start, end):
+        def is_in_range(datapoint):
+            year = self.get_year(datapoint)
+            return year>=start and year<=end
+        data = [d for d in self.get_from_api() if is_in_range(d)]
+        if self.freq=='a':
+            for d in data:
+                d['date'] = self.get_year(d)
+        return dict(x = [d['date'] for d in data],
+                    y = [d['value'] for d in data],
+                    name = self.name)   
+
+print(DataSeries('a', 'GDP_yoy').get_dict(2000,2001))
+
+VARIABLE_NAMES = {freq: VariableNames(freq).as_menu_items()
+                  for freq in ['a', 'q', 'm', 'd']}
+
+def get_names(freq):
+    """Get dropdown menu items."""    
+    return VARIABLE_NAMES.get(freq)
+
 
 def frequencies():
     return [
@@ -48,97 +125,69 @@ def frequencies():
         {'label': 'Monthly', 'value': 'm'},        
         {'label': 'Daily', 'value': 'd'}
     ]
-
     
-BASE_URL = 'http://minikep-db.herokuapp.com/api'
-
-
-def get_from_api_names(freq):
-    url = f'{BASE_URL}/names/{freq}'
-    return requests.get(url).json()
-
-
-def as_menu_items(names):
-    return [{'label': name, 'value': name} for name in names]   
-
-
-# called only once    
-NAMES = {freq: as_menu_items(get_from_api_names(freq))
-         for freq in ['a', 'q', 'm', 'd']}
     
-def get_names(freq):
-    """Get dropdown menu items."""    
-    return NAMES.get(freq)
+MIN_YEAR=1999    
+MAX_YEAR=2018
+
+MARKS = {i: "" for i in range(MIN_YEAR, MAX_YEAR)}
+for year in [MIN_YEAR, 2005, 2010, 2015, MAX_YEAR]:
+    MARKS[year] = str(year)   
 
 
-def get_datapoints_url(freq, name, format):
-    return (f'{BASE_URL}/datapoints'
-            f'?freq={freq}&name={name}&format={format}')    
-    
-
-def get_from_api_datapoints(freq, name):
-    url = get_datapoints_url(freq, name, format='json')
-    data = requests.get(url).json()
-    if not isinstance(data, list):
-         # if parameters are invalid, response is not a jsoned list
-         return []
-    return data
-
-
-def get_time_series_dict(freq, name):
-    data = get_from_api_datapoints(freq, name)
-    return dict(x = [d['date'] for d in data],
-                y = [d['value'] for d in data],
-                name = name)   
-
-# app.layout controls HTML layout of dcc components on page
-
-
-# footer        
-right = dcc.Markdown('''
-#### Github links
-  - [This app code](https://github.com/mini-kep/frontend-dash)
-  - [Proposed enhancements](https://github.com/mini-kep/frontend-dash/blob/master/README.md#proposed-enhancements) 
-  - [Project home](https://github.com/mini-kep/intro) 
-  - [Dev notes](https://github.com/mini-kep/intro/blob/master/DEV.md)    
-''')
-
-# dcc components:
-#  - header and markdown block
+# app.layout controls HTML layout of dcc components on page:
+#  - header and footer markdown blocks
 #  - radio items 
 #  - 2 dropdown menus
 #  - graph with time series
+#  - slider for timerange
 #  - links to download data
-left = html.Div([
-    dcc.Markdown('''
+
+HEADER = '''
 # Explore mini-kep dataset
 
 Use controls below to select time series frequency and variable names.
-'''),
+'''
+
+FOOTER = '''
+**Github links**:
+    
+  - [This app code](https://github.com/mini-kep/frontend-dash)
+  - [Proposed enhancements](https://github.com/mini-kep/frontend-dash/blob/master/README.md#proposed-enhancements) 
+  - [Project home](https://github.com/mini-kep/intro) and
+    [dev notes](https://github.com/mini-kep/intro/blob/master/DEV.md)    
+'''
+
+
+START_VALUES = dict(freq='q', name1='GDP_yoy', name2='CPI_rog')
+
+app.layout = html.Div([
+   dcc.Markdown(HEADER),
    dcc.RadioItems(
+        id='frequency',
         options=frequencies(),
-        value='q',
-        id='frequency'
+        value=START_VALUES['freq']        
     ),
-    dcc.Dropdown(id='name1', value = "GDP_yoy"),
-    dcc.Dropdown(id='name2', value = "CPI_rog"),
-    dcc.Graph(id='time-series-graph'),
-    html.Div(id='download-links') 
+    dcc.Dropdown(id='name1', value = START_VALUES['name1']),
+    dcc.Dropdown(id='name2', value = START_VALUES['name2']),
+    html.Div([            
+            dcc.Graph(id='time-series-graph'),
+            dcc.RangeSlider(
+                id='view-years',    
+                count=1,
+                min=MIN_YEAR, max=MAX_YEAR,
+                step=1,
+                marks=MARKS,
+                value=[MIN_YEAR, MAX_YEAR]
+                )
+        ], style={'marginBottom': '50'}),
+    html.Div(id='download-links', style={'marginBottom': '25'}), 
+    dcc.Markdown(FOOTER),
 ], style={'width': '500', 
-          'marginBottom': 50, 
           'marginTop': 10,
-          'marginLeft': 50, 
-          'marginRight': 50 
+          'marginLeft': 50,
           }
 )
-
-
-app.layout = html.Table([
-    html.Tr([
-            html.Td(left),
-            html.Td(right)
-            ])        
-])
 
 
 @app.callback(output=Output('name1', component_property='options'), 
@@ -157,21 +206,35 @@ def update_names2(freq):
               inputs=[Input('frequency', component_property='value'), 
                       Input('name1', component_property='value'),
                       Input('name2', component_property='value'),                      
+                      Input('view-years', component_property='value'),  
                       ])    
-def update_graph_parameters(freq, name1, name2):
+def update_graph_parameters(freq, name1, name2, years):
     layout_dict = dict(margin = {'l': 40, 'r': 0, 't': 20, 'b': 30},
                        legend = dict(orientation="h"),
-                       showlegend = True)    
-    data_list = [get_time_series_dict(freq, name1)]
+                       showlegend = True)   
+    data_list = [DataSeries(freq, name1).get_dict(*years)]
     if name2:
-        data_list.append(get_time_series_dict(freq, name2))
+        ts2 = DataSeries(freq, name2).get_dict(*years)
+        data_list.append(ts2)
+    # updating x axis based on slider years selection
+    start = years[0]-1
+    end = years[1]+2
+    if freq == 'a':
+       layout_dict['xaxis']=dict(range=["{start}", "{end}"])
+    else:
+       # FIXME: does not seems to work with q-m frequencies         
+       layout_dict['xaxis']=dict(range=[f"{start}-12-31", f"{end}-12-31"])
     return dict(layout=layout_dict, data=data_list) 
+
+# TODO: annotations
+# <https://community.plot.ly/t/annotation-not-showing-on-dash-dcc-graph/6660>
 
 
 def download_html(freq, name):
     link_text = f'{freq}_{name}.csv'
-    url = get_datapoints_url(freq, name, 'csv')
+    url = DataSeries.make_url(freq, name, 'csv')
     return html.A(link_text, href=url)
+
 
 @app.callback(output=Output('download-links', 'children'),
               inputs=[Input('frequency', component_property='value'),
@@ -186,8 +249,6 @@ def update_link_parameters(freq, name1, name2):
         link2 = download_html(freq, name2)
     return ["Download data: ", link1, " ",  link2 ]
 
-# NOT TODO: what tests should be designed for this code?
-#           specifically, how to test for sah components behaviour?
 
 if __name__ == '__main__':
     port = os.environ.get('DASH_PORT', 8000)
