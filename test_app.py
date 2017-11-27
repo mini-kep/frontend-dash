29b7a6a2693767af91d6a90318eef60bf22cc06e
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-11-27 12:17:57 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,11 @@
-from app import DataSeries, URL
+from app import DataSeries
+from access import CustomAPI
 
 def test_DataSeries_filter_limits_variable_length():
     assert DataSeries('a', 'GDP_yoy').filter(2000,2001).dict == \
         {'x': [2000, 2001], 'y': [110.0, 105.1], 'name': 'GDP_yoy'}
         
-def test_URL_custom_link():
-    assert URL(freq="d", name="BRENT").custom_link == \
+def test_CustomAPI_endpoint():
+    ca = access.CustomAPI(freq="d", name="BRENT")
+    assert ca.endpoint == \
         'http://minikep-db.herokuapp.com/all/series/BRENT/d'        
